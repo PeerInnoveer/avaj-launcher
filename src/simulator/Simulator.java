5da639cfbafabb950bc7d42d54ca61611f038cb1
Pierre VAN RENSBURG <pvan-ren@c2r6s4.wethinkcode.co.za>
2019-06-24 16:05:07 +0200

    Almost finished file structure

--- src/simulator/Simulator.java
+++ src/simulator/Simulator.java
@@ -1,31 +1,25 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   Main.java                                          :+:      :+:    :+:   */
+/*   Simulator.java                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: pvan-ren <pvan-ren@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2019/06/21 10:36:37 by pvan-ren          #+#    #+#             */
-/*   Updated: 2019/06/21 14:29:52 by pvan-ren         ###   ########.fr       */
+/*   Created: 2019/06/24 15:01:08 by pvan-ren          #+#    #+#             */
+/*   Updated: 2019/06/24 15:42:46 by pvan-ren         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 import java.io.*;
 
-public class ReadScenario {
-    public static void main(String[] args)throws Exception {
-        File file = new File("../scenario.txt");
+public class Simulator {
+    public static void main(String[] args)throws InterruptedException {
+        File file = new File("../../scenario.txt");
         BufferedReader br = new BufferedReader(new FileReader(file));
-
+        
         String st;
         while ((st = br.readLine()) != null)
         System.out.println(st);
+        br.close();
     }
 }
-
-// public class Main {
-    
-//     public static void main(String[] args) {
-//         System.out.println("Hi there!");
-//     }
-// } 
