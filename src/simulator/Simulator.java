a196e49a8ccccc91d9ee3b1ec300453935367e95
Pierre VAN RENSBURG <pvan-ren@c2r6s4.wethinkcode.co.za>
2019-06-25 15:21:01 +0200

    Update

--- src/simulator/Simulator.java
+++ src/simulator/Simulator.java
@@ -6,7 +6,7 @@
 /*   By: pvan-ren <pvan-ren@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/24 15:01:08 by pvan-ren          #+#    #+#             */
-/*   Updated: 2019/06/25 11:36:09 by pvan-ren         ###   ########.fr       */
+/*   Updated: 2019/06/25 15:16:18 by pvan-ren         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,13 +15,15 @@ import java.io.*;
 class Simulator {
     public static void main(String[] args) {
         try {
-            File file = new File("../../scenario.txt");
+            File file = new File(args[0]);
             BufferedReader br = new BufferedReader(new FileReader(file));
             
-            String line;
-            while ((line = br.readLine()) != null)
-            System.out.println(line);
-            br.close();
+            String line = br.readLine();
+            
+            // System.out.println(line);
+            
+            // br.close();
+            
             
         } catch (Exception e) {
             System.out.println("Something went wrong.");
